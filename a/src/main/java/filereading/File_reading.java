7f30768f90a1af6fbd8c5d47eb0397881a75e3a9
woolenrainbow <janusadr@gmail.com>
2018-06-10 12:11:25 +0200

    interfejs do czytania z plików oraz do gui (gui zacząć od gui_init)

---
+++ a/src/main/java/filereading/File_reading.java
@@ -0,0 +1,7 @@
+package filereading;
+import java.math.*;
+
+public interface File_reading {
+	public BigDecimal[][] read_from_file(char[] path);
+	//do implementacji interfejsu użyć biblioteki new input output, wartość zwracana to dwu wymiarowa tablica bigdecimal (typ z java.math)
+}
